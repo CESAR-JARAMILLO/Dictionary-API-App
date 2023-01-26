64ae3fb0b8b6e429562bb0583f1821c36e4a9e96
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-01-26 14:40:50 -0800

    commented-out code

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-const word = 'keyboard';
-const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+// const word = 'keyboard';
+// const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 
-fetch(url)
-  .then(response => response.json())
-  // .then(data => {
-  //   console.log(data[0].meanings[0].definitions.forEach( i => {
-  //     console.log(i.definition)
-  //   }))
-  // })
-    .then(data => {
-      console.log(data[0].meanings[0])
-      console.log(data[0].word)
-      console.log(data)
-      console.log(data[0].phonetics[2].text)
-      console.log(data[0].phonetics[2].audio)
-      console.log(data[0].meanings.forEach(obj => {
-        console.log(obj.partOfSpeech)
-        console.log(obj.definitions[0].definition)
-        console.log(obj.definitions[0].example)
-      }))
-      console.log(data[0].meanings[0].synonyms)
-      console.log(data[0].sourceUrls)
-    })
-  .catch(error => {
-    console.log(error)
-  });
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data)
+//   })
+//     .then(data => {
+//       console.log(data[0].meanings[0])
+//       console.log(data[0].word)
+//       console.log(data)
+//       console.log(data[0].phonetics[2].text)
+//       console.log(data[0].phonetics[2].audio)
+//       console.log(data[0].meanings.forEach(obj => {
+//         console.log(obj.partOfSpeech)
+//         console.log(obj.definitions[0].definition)
+//         console.log(obj.definitions[0].example)
+//       }))
+//       console.log(data[0].meanings[0].synonyms)
+//       console.log(data[0].sourceUrls)
+//     })
+//   .catch(error => {
+//     console.log(error)
+//   });
